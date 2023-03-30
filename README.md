108b7d62c32e12d13d55e42f8a86b73d2a18d0a9
András Svraka <svraka.andras@gmail.com>
2023-03-30 13:57:22 +0200

    Limit bisect to makedeps

--- README.md
+++ README.md
@@ -9,6 +9,6 @@ First, set up a local mirror for tarballs. In `.env` set its location as `MSYS2_
 Then run a git bisect in the main repo with `bisect.sh` (this calls `test.el` in this repo):
 
 ``` shell
-git bisect start 0b8134337 dbe8d4aa9 -- mingw-w64-emacs mingw-w64-crt-git mingw-w64-headers-git mingw-w64-libmangle-git mingw-w64-tools-git mingw-w64-winpthreads-git mingw-w64-winstorecompat-git
+git bisect start 0b8134337 dbe8d4aa9 -- $(../MINGW-packages-16190/bisect-paths.sh)
 git bisect run ../MINGW-packages-16190/bisect.sh
 ```
---
+++ bisect-paths.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(dirname "$BASH_SOURCE")"  # relative
+SCRIPT_DIR="$(cd "$SCRIPT_DIR" && pwd)" # absolutized and normalized
+
+source "${SCRIPT_DIR}/.env"
+source "${SCRIPT_DIR}/lib.sh"
+
+list_paths() {
+    PACKAGE="$1"
+    PACKAGES=$(get_recursive_package_makedeps "$PACKAGE" | sed -e "s/${MINGW_PACKAGE_PREFIX}-//g")
+    read -d "\034" -r -a PACKAGES <<<"${PACKAGES}\034"
+    declare -a PKGBUILDS
+    for p in "${PACKAGES[@]}"; do
+        p=$(get_pkgbuild_file "$p")
+        PKGBUILDS+=("$p")
+    done
+
+    printf '%s\n' "${PKGBUILDS[@]}" | sort | uniq
+}
+
+list_paths emacs
